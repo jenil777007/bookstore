89d79e7c591ecb8ca45d4d0b8d5625c0dd5a0229
Jenil Calcuttawala <jenilcalcuttawala@gmail.com>
2021-02-13 13:58:06 +0530

    Add support for FireStore

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from firebase_admin import credentials, firestore, initialize_app
+
 app = Flask(__name__)
 
+# Initialize Firestore DB
+cred = credentials.Certificate("key.json")
+default_app = initialize_app(cred)
+db = firestore.client()
+books_ref = db.collection("books")
 
-@app.route('/')
+
+@app.route("/api/books")
 def hello():
-    return "Hello World!"
+    return {"data": "string"}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
